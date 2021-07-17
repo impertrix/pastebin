8109af6e14891071805747bd65122a4929980f71
Matthew <nicetry.ihatemyself.xyz>
2021-07-17 16:31:49 -0700

    health check

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,13 @@
 import { Module, CacheModule } from '@nestjs/common';
 import { PasteModule } from './paste/paste.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ThrottlerModule } from '@nestjs/throttler';
 @Module({
   imports: [
     PasteModule,
     MongooseModule.forRoot(process.env.MONGO_URI),
     CacheModule.register(),
+    ThrottlerModule.forRoot({ttl: 60, limit: 50})
   ],
   controllers: [],
   providers: [],
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,15 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, InternalServerErrorException } from '@nestjs/common';
+import { InjectConnection } from '@nestjs/mongoose';
+import { Connection } from 'mongoose';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  constructor(@InjectConnection() private readonly connection: Connection) {}
+  getHealth(): string {
+    if (this.connection.readyState === 1) {
+      return 'healthy';
+    } else {
+      throw new InternalServerErrorException();
+    }
   }
 }
